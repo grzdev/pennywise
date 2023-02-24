da83a1ce0e30e147cd11c2b7ccd12cef0566bbbe
zay <damilolaoyeniyi13@gmail.com>
2023-02-24 01:32:10 +0100

    Addition of fade to other elements

--- modules/Dashboard/components/DashBoardLandingPage.tsx
+++ modules/Dashboard/components/DashBoardLandingPage.tsx
@@ -194,7 +194,6 @@ const DashBoardLandingPage = () => {
               <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
             </PieChart>
           </Flex>
-
           <Flex
             mt={["","","2.6rem","-2rem"]}
             >
